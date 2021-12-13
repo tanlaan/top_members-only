9b7a4d8c0f2b889c90f72b15e47c58e526422a2b
Chris Bolas <tanlaan33@gmail.com>
2021-12-12 17:38:46 -0800

    Override Devise defaults in RegistrationController

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+class RegistrationsController < Devise::RegistrationsController
+  private
+  
+  def sign_up_params
+    params.require(:user).permit(:name, :username, :email, :password, :pssword_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :username, :email, :password, :pssword_confirmation, :current_password)
+  end
+
+end
